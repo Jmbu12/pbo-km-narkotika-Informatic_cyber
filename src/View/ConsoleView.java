e1658cd2998d40b7b92dd1e0c2767cea1dcbb021
najmul akhyar <najmulakhyar@webmail.umm.ac.id>
2026-07-01 22:35:04 +0700

    feat: menambahkan class ConsoleView

---
+++ src/View/ConsoleView.java
@@ -0,0 +1,18 @@
+package view;
+
+import model.Putusan;
+import model.StatistikPutusan;
+import util.InputHandler;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ConsoleView {
+
+    private final Scanner scanner;
+
+    public ConsoleView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    }
