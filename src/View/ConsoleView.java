2acada885d7f6b5d2a0f73e2c9db68150c7cef60
najmul akhyar <najmulakhyar@webmail.umm.ac.id>
2026-07-01 22:38:32 +0700

    feat: menambahkan menu utama

--- src/View/ConsoleView.java
+++ src/View/ConsoleView.java
@@ -21,3 +21,17 @@ public class ConsoleView {
         System.out.println("   PUTUSAN PENGADILAN NARKOTIKA");
         System.out.println("=========================================================");
     }
+
+    public int tampilkanMenu() {
+        System.out.println("\n----------------- MENU UTAMA -----------------");
+        System.out.println("1. Tambah Putusan Baru");
+        System.out.println("2. Tampilkan Semua Putusan");
+        System.out.println("3. Cari Putusan (Nomor / Nama)");
+        System.out.println("4. Filter Putusan (Jenis / Pengadilan / Vonis)");
+        System.out.println("5. Hapus Putusan");
+        System.out.println("6. Tampilkan Statistik");
+        System.out.println("7. Urutkan Data (Vonis / Denda)");
+        System.out.println("8. Keluar");
+        System.out.println("-----------------------------------------------");
+        return InputHandler.validasiPilihan("Pilih menu (1-8): ", 1, 8, scanner);
+    }
