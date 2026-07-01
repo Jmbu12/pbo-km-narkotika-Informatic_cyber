2741129b862e0d545ec262ecec3d25f9110b3320
erwinsusilowanto08-cloud <erwinsusilowanto08@webmail.umm.ac.id>
2026-07-01 14:42:03 +0700

    feat:menambahkan Konstruktor di class PutusanNarkotikaBerat

--- src/model/PutusanNarkotikaBerat.java
+++ src/model/PutusanNarkotikaBerat.java
@@ -2,4 +2,21 @@ package model;
 
 public class PutusanNarkotikaBerat extends Putusan {
 
+    private boolean terindikasiJaringan; // true jika diduga bagian dari jaringan/sindikat
+    private String catatanKhusus;
+
+    public PutusanNarkotikaBerat() {
+        super();
+        this.terindikasiJaringan = false;
+        this.catatanKhusus = "-";
+    }
+    public PutusanNarkotikaBerat(String nomorPerkara, String pengadilan, String tanggalPutusan,
+                                 String namaTerdakwa, int umurTerdakwa, String jenisNarkotika,
+                                 double beratBarangBukti, String pasalDilanggar, String peranTerdakwa,
+                                 int vonisHukuman, double vonisDenda, String namaHakim,
+                                 boolean terindikasiJaringan, String catatanKhusus) {
+        super(nomorPerkara, pengadilan, tanggalPutusan, namaTerdakwa, umurTerdakwa, jenisNarkotika,
+                beratBarangBukti, pasalDilanggar, peranTerdakwa, vonisHukuman, vonisDenda, namaHakim);
+        this.terindikasiJaringan = terindikasiJaringan;
+        this.catatanKhusus = catatanKhusus;
     }
