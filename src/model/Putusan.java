81cf5dc83221178b7567a72cff5452e3b06aca3b
erwinsusilowanto08-cloud <erwinsusilowanto08@webmail.umm.ac.id>
2026-07-01 12:57:03 +0700

    feat:menambahkan class Putusan

---
+++ src/model/Putusan.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Putusan implements Dapatdicetak {
+
+    private String nomorPerkara;
+    private String pengadilan;
+    private String tanggalPutusan;
+    private String namaTerdakwa;
+    private int umurTerdakwa;
+    private String jenisNarkotika;
+    private double beratBarangBukti; // gram
+    private String pasalDilanggar;
+    private String peranTerdakwa;
+    private int vonisHukuman; // bulan
+    private double vonisDenda; // rupiah
+    private String namaHakim;
