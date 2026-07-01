e9cba8ff2e9257b04d3932a9d81826a14484260e
erwinsusilowanto08-cloud <erwinsusilowanto08@webmail.umm.ac.id>
2026-07-01 13:10:35 +0700

    feat:menambahkan private static int and public Putusan di class Putusan

--- src/model/Putusan.java
+++ src/model/Putusan.java
@@ -14,3 +14,21 @@ public class Putusan implements Dapatdicetak {
     private int vonisHukuman; // bulan
     private double vonisDenda; // rupiah
     private String namaHakim;
+
+    private static int jumlahDibuat = 0;
+
+    public Putusan() {
+        this.nomorPerkara = "BELUM DIISI";
+        this.pengadilan = "BELUM DIISI";
+        this.tanggalPutusan = "BELUM DIISI";
+        this.namaTerdakwa = "BELUM DIISI";
+        this.umurTerdakwa = 0;
+        this.jenisNarkotika = "BELUM DIISI";
+        this.beratBarangBukti = 0.0;
+        this.pasalDilanggar = "BELUM DIISI";
+        this.peranTerdakwa = "BELUM DIISI";
+        this.vonisHukuman = 0;
+        this.vonisDenda = 0.0;
+        this.namaHakim = "BELUM DIISI";
+        jumlahDibuat++;
+    }
